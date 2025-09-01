f4e5c2f22dbcc29bd650c18fcd6b20240d88cad6
eduardcod <32468387+eduardcod@users.noreply.github.com>
2025-09-01 10:24:23 -0300

    Update Install-GLPI-Agent.sh

--- Install-GLPI/Install-GLPI-Agent.sh
+++ Install-GLPI/Install-GLPI-Agent.sh
@@ -10,7 +10,7 @@ LOG_FILE="$HOME/glpi-agent-install.log"
 GLPI_SERVER="http://10.123.87.5/marketplace/glpiinventory/"
 AGENT_TAG="PDV"
 GLPI_VERSION="1.15"
-INSTALLER="$INST/glpi-agent-1.15-x86_64.AppImage"
+INSTALLER="$HOME/glpi-agent-1.15-x86_64.AppImage"
 
 # Redirecionar saída para log e terminal
 exec > >(tee -a "$LOG_FILE") 2>&1
